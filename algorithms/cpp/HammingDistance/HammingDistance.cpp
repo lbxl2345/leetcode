8f7a3e1f88f7eb29c26c9532f837ca8fb526bfed
lbxl2345 <727934274@qq.com>
2017-02-05 16:41:32 +0800

    first commit

--- algorithms/cpp/HammingDistance/HammingDistance.cpp
+++ algorithms/cpp/HammingDistance/HammingDistance.cpp
@@ -12,7 +12,8 @@ public:
     }
 };
 //更好的方法
-//
+//temp是x、y异或的结果，其中包含多少个1，就说明有多少位是不同的
+//temp &= temp-1；相当于每次从temp中拿走了一个1，因此最后能够拿走几次 就能够统计出1的个数
 class Solution {
 public:
     int hammingDistance(int x, int y) {
